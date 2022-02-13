8807126e4572a8657d8cfc24e8a0146894a547cd
lululuweihai <70012670+lululuweihai@users.noreply.github.com>
2022-02-13 14:00:13 +0800

    qwe

--- script/createRuntime.bat
+++
@@ -1,1 +0,0 @@
-jlink --strip-debug --compress 1 --no-header-files --no-man-pages --output runtime --add-modules java.base,java.logging,java.desktop,java.scripting,java.xml,jdk.unsupported,java.instrument,java.management,java.naming,java.prefs,java.rmi,java.scripting,java.sql,java.xml,jdk.httpserver
--- script/winLaunch.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set BAT_HOME = ./runtime/bin
-java -jar LwhResourceManager.jar -Xmx128M -Xms64M -Xmn32M --illegal-access=deny --add-opens java.base/java.lang.reflect=ALL-UNNAMED
